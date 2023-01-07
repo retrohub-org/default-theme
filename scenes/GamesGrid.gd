b11468cf1c82c6fdaeb092d2b724343b1628a5ee
Ricardo Subtil <ricasubtil@gmail.com>
2023-01-07 18:09:49 +0000

    Implement sort/view/preview options

--- scenes/GamesGrid.gd
+++ scenes/GamesGrid.gd
@@ -1,29 +1,105 @@
-extends HFlowContainer
+extends Control
 
 signal show_game_info(game_data)
+
+onready var n_grid := $"%Grid"
+onready var n_no_games := $"%NoGamesLabel"
+
+var system_data : RetroHubSystemData
+
+var _sort_method : int = -1
+
+static func sort_alphabetically(a: Node, b: Node):
+	var a_data : RetroHubGameData = a.game_data
+	var b_data : RetroHubGameData = b.game_data
+	if RetroHubConfig.get_theme_config("sort_reversed", false):
+		return a_data.name.naturalnocasecmp_to(b_data.name) == -1
+	return b_data.name.naturalnocasecmp_to(a_data.name) == -1
+
+static func sort_playdate(a: Node, b: Node):
+	var a_data : RetroHubGameData = a.game_data
+	var b_data : RetroHubGameData = b.game_data
+	var a_date := 0 if a_data.last_played == "null" else \
+		int(a_data.last_played.substr(0, 8) + a_data.last_played.substr(10))
+	var b_date := 0 if b_data.last_played == "null" else \
+		int(b_data.last_played.substr(0, 8) + b_data.last_played.substr(10))
+	if RetroHubConfig.get_theme_config("sort_reversed", false):
+		return a_date > b_date
+	return b_date > a_date
+
+static func sort_playcount(a: Node, b: Node):
+	var a_data : RetroHubGameData = a.game_data
+	var b_data : RetroHubGameData = b.game_data
+	if RetroHubConfig.get_theme_config("sort_reversed", false):
+		return a_data.play_count > b_data.play_count
+	return b_data.play_count > a_data.play_count
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	RetroHub.connect("game_received", self, "_on_game_received")
 
 func _on_game_received(data: RetroHubGameData):
+	if data.system != system_data:
+		return
+
 	var button
 	if data.has_media:
 		button = preload("res://scenes/game_box/GameBoxImage.tscn").instance()
 	else:
 		button = preload("res://scenes/game_box/GameBoxSimple.tscn").instance()
-	add_child(button)
+	n_grid.add_child(button)
 	button.game_data = data
 	button.connect("show_game_info", self, "_on_show_game_info")
 
 func _on_show_game_info(data: RetroHubGameData):
 	emit_signal("show_game_info", data)
 
+func sort_children(sort_method: int, force_sort: bool):
+	# Sorting is costly, only do it if there's a change
+	if !force_sort and _sort_method == sort_method:
+		return
 
-func _on_SystemBar_system_selected(data: RetroHubSystemData, should_focus: bool):
+	_sort_method = sort_method
+	# Copy existing children and remove them from the node
+	var children := n_grid.get_children()
+	# Now sort children array
+	match _sort_method:
+		1: # By Last Played
+			children.sort_custom(self, "sort_playdate")
+		2: # By Play Count
+			children.sort_custom(self, "sort_playcount")
+		0, _: # Alphabetically or unknown
+			children.sort_custom(self, "sort_alphabetically")
+	# Finally, read them to the node
+	var idx = 0
+	for child in children:
+		n_grid.move_child(child, 0)
+		idx += 1
+
+func filter_children(filter_method: int):
+	var any_visible := false
+	for child in n_grid.get_children():
+		match filter_method:
+			1: # Only favorites
+				child.visible = child.game_data.favorite
+			2: # Only played before
+				child.visible = child.game_data.play_count > 0
+			3: # Only with media
+				child.visible = child.game_data.has_media
+			0: # All, or unknown
+				child.visible = true
+		any_visible = any_visible or child.visible
+	n_no_games.set_visible(!any_visible)
+
+func set_preview_mode(preview_mode: int):
+	for child in n_grid.get_children():
+		child.set_preview_mode(preview_mode)
+
+func system_selected(should_focus: bool):
+	filter_children(RetroHubConfig.get_theme_config("filter_mode", 0))
+	sort_children(RetroHubConfig.get_theme_config("sort_mode", 0), true)
 	var selected = false
-	for child in get_children():
-		child.visible = child.game_data.system == data
-		if should_focus and child.visible and not selected:
+	for child in n_grid.get_children():
+		if not selected and should_focus and child.visible:
 			child.grab_focus()
 			selected = true
--- scenes/game_box/GameBoxImage.gd
+++ scenes/game_box/GameBoxImage.gd
@@ -3,9 +3,13 @@ extends Button
 signal show_game_info(game_data)
 
 onready var n_media := $"%Media"
+onready var n_video := $"%Video"
+onready var n_anim := $"%Anim"
 onready var n_label := $"%Label"
 onready var n_more_info_root := $"%MoreInfoRoot"
 onready var n_play_container := $"%PlayContainer"
+
+var _preview_mode : int
 
 var game_data : RetroHubGameData setget set_game_data
 
@@ -26,19 +30,24 @@ func _on_app_returning(_unused: RetroHubSystemData, data: RetroHubGameData):
 		_on_GameBoxImage_focus_entered()
 
 func _on_media_loaded(media_data: RetroHubGameMediaData, _game_data: RetroHubGameData, types: int):
-	if game_data == _game_data and not n_media.texture:
-		if types == RetroHubMedia.Type.TITLE_SCREEN:
-			if media_data.title_screen:
-				n_media.texture = media_data.title_screen
-			else:
-				RetroHubMedia.retrieve_media_data_async(game_data, RetroHubMedia.Type.SCREENSHOT)
-		elif types == RetroHubMedia.Type.SCREENSHOT:
-			if media_data.screenshot:
-				n_media.texture = media_data.screenshot
-			else:
-				RetroHubMedia.retrieve_media_data_async(game_data, RetroHubMedia.Type.LOGO)
-		elif types == RetroHubMedia.Type.LOGO and media_data.logo:
-			n_media.texture = media_data.logo
+	if game_data == _game_data:
+		if (types & RetroHubMedia.Type.VIDEO) and media_data.video:
+			n_video.stream = media_data.video
+			if has_focus():
+				n_anim.play("Fade In Video")
+		if not n_media.texture:
+			if types == RetroHubMedia.Type.TITLE_SCREEN:
+				if media_data.title_screen:
+					n_media.texture = media_data.title_screen
+				elif _preview_mode == 0:
+					RetroHubMedia.retrieve_media_data_async(game_data, RetroHubMedia.Type.SCREENSHOT)
+			elif types == RetroHubMedia.Type.SCREENSHOT:
+				if media_data.screenshot:
+					n_media.texture = media_data.screenshot
+				elif _preview_mode == 0:
+					RetroHubMedia.retrieve_media_data_async(game_data, RetroHubMedia.Type.LOGO)
+			elif types == RetroHubMedia.Type.LOGO and media_data.logo:
+				n_media.texture = media_data.logo
 
 func _on_game_data_updated(data: RetroHubGameData):
 	if game_data == data:
@@ -51,21 +60,48 @@ func _on_GameBoxImage_focus_entered():
 	RetroHub.set_curr_game_data(game_data)
 	n_more_info_root.visible = true
 	n_play_container.visible = true
+	if RetroHubConfig.get_theme_config("preview_video", true):
+		if n_video.stream:
+			n_anim.play("Fade In Video")
+		else:
+			RetroHubMedia.retrieve_media_data_async(game_data, RetroHubMedia.Type.VIDEO)
 
 func _on_GameBoxImage_focus_exited():
 	n_more_info_root.visible = false
 	n_play_container.visible = false
+	n_anim.play("RESET")
 
 func _gui_input(event):
 	if event.is_action_pressed("rh_major_option"):
+		get_tree().set_input_as_handled()
 		_on_MoreInfo_pressed()
 
 func _on_GameBoxImage_visibility_changed():
-	if visible and not n_media.texture:
-		RetroHubMedia.retrieve_media_data_async(game_data, RetroHubMedia.Type.TITLE_SCREEN)
+	_preview_mode = RetroHubConfig.get_theme_config("preview_mode", 0)
+	if is_visible_in_tree() and not n_media.texture:
+		RetroHubMedia.retrieve_media_data_async(game_data, get_internal_preview_type())
 	else:
 		RetroHubMedia.cancel_media_data_async(game_data)
 		n_media.texture = null
+		if n_video.stream:
+			n_video.stream = null
 
 func _on_MoreInfo_pressed():
 	emit_signal("show_game_info", game_data)
+
+func get_internal_preview_type():
+	match _preview_mode:
+		2: # Screenshot
+			return RetroHubMedia.Type.SCREENSHOT
+		3: # Game Logo
+			return RetroHubMedia.Type.LOGO
+		4: # Nothing
+			return 0
+		0, 1, _: # Auto, title screen or unknown
+			return RetroHubMedia.Type.TITLE_SCREEN
+
+func set_preview_mode(preview_mode: int):
+	if _preview_mode == preview_mode:
+		return
+	n_media.texture = null
+	_on_GameBoxImage_visibility_changed()
