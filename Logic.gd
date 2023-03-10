13dfba3d45ec2908e88229c802cfc445439f313a
Ricardo Subtil <ricasubtil@gmail.com>
2023-03-10 18:34:56 +0000

    Fix games not updating when config is changed

--- Logic.gd
+++ Logic.gd
@@ -7,8 +7,13 @@ onready var n_bottom_info := $"%BottomInfo"
 func _ready():
 	n_bottom_info.visible = false
 	RetroHub.connect("game_receive_start", self, "_on_game_receive_start")
+	RetroHubConfig.connect("config_updated", self, "_on_config_updated")
 
 func _on_game_receive_start():
 	remove_child(n_no_games)
 	n_no_games.queue_free()
 	n_bottom_info.visible = true
+
+func _on_config_updated(key: String, old, new):
+	if key == ConfigData.KEY_GAMES_DIR:
+		RetroHub.request_theme_reload()
